ce40a098ec8aa4cfda6f31a8324f55affefafaac
Matis GASSE <matis.gasse@lyceefulbert.fr>
2023-09-27 16:54:39 +0200

    essai image

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -35,6 +35,9 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(mappedBy: 'utilisateur', targetEntity: Article::class)]
     private Collection $listeArticles;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
     public function __construct()
     {
         $this->listeArticles = new ArrayCollection();
@@ -212,6 +215,18 @@ class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
     }
 
 }
