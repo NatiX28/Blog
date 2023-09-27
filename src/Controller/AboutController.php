90c84e6771c1e38b56c8e51bd85ee8cfc8bba6de
Matis GASSE <matis.gasse@lyceefulbert.fr>
2023-09-27 16:02:53 +0200

    about

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class AboutController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
+    #[Route('/about', name: 'app_about')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
         ]);
     }
-
 }
